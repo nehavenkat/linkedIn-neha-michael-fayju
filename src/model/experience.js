28ef915b653197ea32f6277f69c6b55679ecaef4
vennhost <samogconsult@gmail.com>
2020-01-20 16:14:30 +0100

    experience CRUD

---
+++ src/model/experience.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+var uniqueValidator = require('mongoose-unique-validator');
+
+const expSchema = new Schema({
+    role: {
+        type: String,
+        required: [true, "Role is required!"]
+        
+    },
+    company: {
+        type: String,
+        required: [true, "Company is required!"],
+        minlength: 3,
+        maxlength: 150
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String
+        
+    },
+    area: {
+        type: String
+    },
+    username: {
+        type: String,
+        unique: [true, "username already exist in database"]
+    },
+    image: {
+        type: String,
+        default: "http://via.placeholder.com/360x360"
+    }
+}, { timestamps: true });
+
+expSchema.plugin(uniqueValidator);
+
+const Exp = mongoose.model("Experience", expSchema);
+
+module.exports = Exp;
+
+/* EXPERIENCE Model:
+    {
+        "_id": "5d925e677360c41e0046d1f5",  //server generated
+        "role": "CTO",
+        "company": "Strive School",
+        "startDate": "2019-06-16T22:00:00.000Z",
+        "endDate": "2019-06-16T22:00:00.000Z", //could be null
+        "description": "Doing stuff here and there",
+        "area": "Berlin",
+        "username": "admin",
+        "createdAt": "2019-09-30T19:58:31.019Z",  //server generated
+        "updatedAt": "2019-09-30T19:58:31.019Z",  //server generated
+        "image": ... //server generated on upload, set a default here
+    }  */
--- src/route/experience.js
+++ src/route/experience.js
@@ -1,27 +1,106 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
+const Exp = require("../model/experience")
+const { Transform } = require("json2csv")
+const fs = require("fs-extra")
+const path = require("path")
 
 
 router.get('/', async (req, res) => {
-    res.send('get all experiences')
+
+    try {
+        const exps = await Exp.find()
+        res.send(exps)
+
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+
 })
 router.get('/:expId', async (req, res) => {
-    res.send('get experiences for single ID')
+    try {
+        const exp = await Exp.findById(req.params.expId)
+        res.send(exp)
+
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
 })
 router.post('/', async (req, res) => {
-    res.send('POST  a new experience')
+
+    const newExp = await Exp.create({ ...req.body, createAt: new Date(), updatedAt: new Date() })
+
+    newExp.save()
+    res.send(newExp)
 })
 router.put('/:expId', async (req, res) => {
-    res.send('update  an experience by ID')
+    try {
+        const editExp = await Exp.findByIdAndUpdate(req.params.expId, { $set: { ...req.body } })
+
+        res.send(editExp)
+
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+
 })
 router.delete('/:expId', async (req, res) => {
-    res.send('DELETE an experience by ID')
+
+    try {
+        const deleteExp = await Exp.findByIdAndDelete(req.params.expId)
+
+        res.send('Deleted Successfully')
+
+    } catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+
 })
 router.post('/:expId/picture', async (req, res) => {
     res.send('POST new picture')
 })
-router.get('/:expId/CSV', async (req, res) => {
-    res.send('POST CSV')
-})
+router.get('/:expId/csv', async (req, res) => {
+    const filePath = path.join(__dirname, "../", "public", "exports", "csv-" + req.params.expId + ".csv")
+    const exp = await Exp.findById(req.params.expId)
+   
+    const fields = ["_id", "role", "company", "startDate", "endDate", "username"]
+    //const opts = { fields }
 
-module.exports = router;
+    try {
+       const csv = json2csv(exp, { fields });
+    } catch (err) {
+        return res.status(500).json({ err });
+    }
+
+    fs.writeFile(filePath, csv, function (err) {
+        if (err) {
+            return res.json(err).status(500);
+        }
+        else {
+            setTimeout(function () {
+                fs.unlink(filePath, function (err) { // delete this file after 30 seconds
+                    if (err) {
+                        console.error(err);
+                    }
+                    console.log('File has been Deleted');
+                });
+
+            }, 30000);
+            res.download(filePath);
+        }
+        /*   try {
+              const json2csv = new Transform(opts)
+              await fs.createReadStream(exp).pipe(json2csv).pipe(res)
+              
+          } catch (error) {
+              console.log(error)
+              res.send(error)
+          } */
+        })
+    })
+
+    module.exports = router;
