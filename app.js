6fd0b22fd2339e3de37d7c30ad68bc59018467d6
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-20 11:20:17 +0100

    made connection to mongodb

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
-const listEndpoints = require('express-list-endpoints')
-require('dotenv').config()
+const mongooseConnection = require("./src/db/mongoose");
+const listEndpoints = require("express-list-endpoints");
+require("dotenv").config();
 const port = process.env.PORT;
-const experienceRouter = require('./src/route/experience')
-const profileRouter = require('./src/route/profile')
-const postRouter = require('./src/route/post')
+mongooseConnection();
+const experienceRouter = require("./src/route/experience");
+const profileRouter = require("./src/route/profile");
+const postRouter = require("./src/route/post");
 
 app.get("/", (req, res) => res.send("LinledIn Profile"));
 
-app.use('/experiences', experienceRouter)
-app.use('/profiles', profileRouter)
-app.use('/posts', postRouter)
+app.use("/experiences", experienceRouter);
+app.use("/profiles", profileRouter);
+app.use("/posts", postRouter);
 
 console.log(listEndpoints(app));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/db/mongoose.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+var mongoose = require("mongoose");
+const mongooseConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000
+    })
+    .then(
+      () => {
+        console.log("Connected!");
+      },
+      err => {
+        console.log(err.reason);
+      }
+    );
+};
+
+module.exports = mongooseConnection;
