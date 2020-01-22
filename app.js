3985246cf2550d9a8aedad9b63e8c2f21fc19e98
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-01-22 16:11:41 +0100

    Added Cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
@@ -13,11 +14,23 @@ const postRouter = require("./src/route/post");
 
 //app.use(bodyParser.json());
 
+var whitelist = ["http://localhost:3000", "https://faizanbardai.github.io"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("LinkedIn Profile"));
 
-app.use("/images", express.static(path.join(__dirname, "images")))
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/experiences", experienceRouter);
 app.use("/profiles", profileRouter);
 app.use("/posts", postRouter);
