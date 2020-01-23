66960e4759bd2db7e9eb48ac2fe8690fa56b6bb8
vennhost <samogconsult@gmail.com>
2020-01-23 11:16:52 +0100

    user route

---
+++ src/route/user.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const Profile = require("../model/profile");
+const { check, validationResult } = require("express-validator");
+const User = require("../model/user")
+
+router.post("/", async (req, res) => {
+    try {
+        const newUser = await User.create({ ...req.body })
+        newUser.save()
+        res.send(newUser)
+        
+    } catch (error) {
+      console.log(error)
+      res.send(error)  
+    }
+})
