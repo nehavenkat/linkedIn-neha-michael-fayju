096d89bb17a71645f7e97d69bd0f4d98a8819dfc
vennhost <samogconsult@gmail.com>
2020-01-24 12:46:01 +0100

    Comment

--- src/route/comment.js
+++ src/route/comment.js
@@ -15,8 +15,35 @@ router.post("/", async (req, res) => {
 })
 
 router.get("/", async (req, res) => {
-    const comments = await Comment.find()
-    res.send(comments)
+    try {
+        const comments = await Comment.find()
+        res.send(comments)
+        
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+        const newComment = await Comment.findByIdAndUpdate(req.params.id, { $set: {...req.body} }, {new: true})
+    
+        res.send(newComment)
+        
+    } catch (error) {
+      console.log(error)
+      res.send(error)  
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const comments = await Comment.findByIdAndDelete(req.params.id)
+        res.send(comments)
+        
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 module.exports = router;
